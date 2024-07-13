b0768b681c73121989bcacf06e72329e524ab6cd
gmwendel <28656471+gmwendel@users.noreply.github.com>
2024-07-13 00:36:06 -0400

    Update dependencies in setup script

--- setup.py
+++ setup.py
@@ -1,42 +1,36 @@
-# -*- coding: utf-8 -*-
-
 """
 Installation script
 """
 
-from __future__ import absolute_import
-
-from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import setup, find_packages
 
-
-def get_dist(pkgname):
-    try:
-        return get_distribution(pkgname)
-    except DistributionNotFound:
-        return None
-
+# List of required packages
 install_requires = [
     "matplotlib>=3.0",
     "scipy",
-    "numpy>=1.19.2",
+    "numpy",
     "pandas",
-    "seaborn"
+    "seaborn",
+    "tensorflow>=2.16"  # Default to CPU version of TensorFlow
 ]
 
+# Optional dependencies
+extras_require = {
+    "gpu": ["tensorflow[and-cuda]>=2.16"],
+}
+
+# Setup configuration
 setup(
     name="PLSE",
-    description=(
-        "A simple 1D CNN to count the number of photoelectrons in a waveform"
-    ),
+    description="A simple 1D CNN to count the number of photoelectrons in a waveform",
     author="Garrett Wendel et al.",
     author_email="gmw5164@psu.edu",
     url="tbd",
     license="Apache 2.0",
     version="0.0",
-    python_requires=">=3.7",
-    setup_requires=["tensorflow"],
+    python_requires=">=3.10",
     install_requires=install_requires,
+    extras_require=extras_require,
     packages=find_packages(),
     zip_safe=False,
     entry_points={
